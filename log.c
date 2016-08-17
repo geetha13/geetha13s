3c7d7cc77d60e53ad470854233ba60d5edac4257
geetha13 <j.geethakumar92@gmail.com>
2016-08-17 17:04:15 +0530

    log.c

---
+++ log.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib>
+void print(int arr[]),int size)
+{
+int i,j;
+printf("reporting elements are");
+for(i=0;i<size;i++)
+for(j=j+1;j<size;j++)
+if(arr[i]==arr[j])
+printf("%d",arr[i]);
+}
+int main()
+{
+int arr[]={4,2,4,5,2,3,1};
+int arr-size=size of(arr);
+printf(arr,arr_size);
+getch();
+return 0;
+}
